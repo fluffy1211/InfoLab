465876501a4d768dec7a224856df9d635dc524b9
Gabriel Martin <gabrielmartin13009@gmail.com>
2024-01-21 17:54:39 +0100

    je prie

--- header.php
+++ header.php
@@ -5,6 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Site de Vente de Pièces d'Ordinateurs</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Goldman&display=swap" rel="stylesheet">
+
     
 </head>
 <body>
