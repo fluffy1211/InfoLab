5d37479a0e431ee930a59f7d8c9d3b9362c15bad
Zacdxkun <zacdevouche@gmail.com>
2024-01-21 19:04:26 +0100

    dernier push

--- homepage.php
+++ homepage.php
@@ -3,19 +3,19 @@
 include_once 'header.php';
 
 ?>
+<div class="img-home">
+    <div class="pchome"> 
+        <a href="ordinateurs.php"><img  src="Images\pc-gamer.png" alt=""></a>
+        <h2><a href="ordinateurs.php">Nos Ordinateurs !</a></h2>
+    </div>
 
-<div class="pchome"> 
-    <a href="ordinateurs.php"><img  src="Images\pc-gamer.png" alt=""></a>
-    <h2><a href="ordinateurs.php">Nos Ordinateurs !</a></h2>
+
+
+    <div class="gpuhome"> 
+        <a href="composants.php"><img  src="Images\gpu-homepage.png" alt=""></a>
+        <h2><a href="composants.php">Nos Composants !</a></h2>
+    </div>
 </div>
-
-
-
-<div class="gpuhome"> 
-    <a href="composants.php"><img  src="Images\gpu-homepage.png" alt=""></a>
-    <h2><a href="composants.php">Nos Composants !</a></h2>
-</div>
-
 <footer>
     © 2024 InfoLAB
 </footer>
